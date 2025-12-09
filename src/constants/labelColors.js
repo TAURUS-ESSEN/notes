234b195677e7ce244b186d2eb28f7994e7c1a3b4
TAURUS-ESSEN <reitarovea@gmail.com>
2025-12-09 22:50:05 +0100

    style bg-color-500 -> bg-color-400 for labels

--- src/constants/labelColors.js
+++ src/constants/labelColors.js
@@ -1,17 +1,17 @@
 export const LABEL_COLOR_CLASSES = {
-    amber:   'bg-amber-500 text-white',
-    rose:    'bg-rose-500 text-white',
-    green:   'bg-green-500 text-white',
-    blue:    'bg-blue-500 text-white',
-    purple:  'bg-purple-500 text-white',
-    indigo:  'bg-indigo-500 text-white',
+    amber:   'bg-amber-400 text-white',
+    rose:    'bg-rose-400 text-white',
+    green:   'bg-green-400 text-white',
+    blue:    'bg-blue-400 text-white',
+    purple:  'bg-purple-400 text-white',
+    indigo:  'bg-indigo-400 text-white',
 };
 
 export const LABEL_COLOR_CLASSES_TEXT = {
-    amber:   'text-amber-500',
-    rose:    'text-rose-500',
-    green:   'text-green-500 ',
-    blue:    'text-blue-500 ',
-    purple:  'text-purple-500 ',
-    indigo:  'text-indigo-500 ',
+    amber:   'text-amber-400',
+    rose:    'text-rose-400',
+    green:   'text-green-400 ',
+    blue:    'text-blue-400 ',
+    purple:  'text-purple-400 ',
+    indigo:  'text-indigo-400 ',
 };
