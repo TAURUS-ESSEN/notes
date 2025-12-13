787de7996c019cd3d4cbefb7387dbd9484c74586
TAURUS-ESSEN <reitarovea@gmail.com>
2025-12-13 12:54:35 +0100

    feat: extract Notecard component

--- src/pages/Trash.jsx
+++ src/pages/Trash.jsx
@@ -1,8 +1,6 @@
-import {Link} from 'react-router-dom'
 import { useAppContext } from "../components/AppContext";
 import {useSortedNotes} from '../hooks/useSortedNotes'
-import NoteCardButtons from './NoteCardButtons';
-import NoteLabelList from '../components/NoteLabelList';
+import NoteCard from '../components/NoteCard';
 
 export default function Trash() {
     const {labels, notes, setNotes, setToasts} = useAppContext()
@@ -16,20 +14,8 @@ export default function Trash() {
     return (
         <div className="h-screen w-full relative p-4 ">
             <div className='flex flex-wrap gap-4  '>
-            {notes.length > 0 && sortedNotes.map(note=> ( 
-                
-                    <div key={note.id} className='notePreviewContainer group'>
-                        <NoteCardButtons note={note}  undo={(id, toastId) => undoRestore(id, note.deletedAt, toastId)}/>
-
-                        <Link to={`/edit/${note.id}`}  title='Click to edit this note' className='flex flex-1'>
-                            <div className='notePreview'>
-                                <div className='line-clamp-2 font-semibold break-all text-(--title-card)'>{note.title} </div>
-                                <div className='text-sm line-clamp-3 break-all text-(--text-card)'> { note.text }</div>
-                            </div>
-                        </Link>
-                        <NoteLabelList labels={labels} note={note} />    
-                    </div> 
-                )
+            {notes.length > 0 && sortedNotes.map(note=> 
+                <NoteCard note={note} labels={labels} undo={undoRestore} key={note.id}/>
             )}
             {sortedNotes.length === 0 && 
                 <div className='flex  mt-10 justify-center flex-1 text-xl text-(--text-default)'>
